9560728ba0d2df82c8f283581fbbeee379f133b0
QwertyCode <167351530+QwertyCodeQC@users.noreply.github.com>
2025-03-21 22:27:08 +0100

    content add v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,18 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
+			head: [
+				{tag: 'script', attrs: {src: 'https://buttons.github.io/buttons.js', defer: true}},
+			],
+			favicon: '/favicon.svg',
 			title: 'Astra',
 			social: {
 				npm: 'https://www.npmjs.com/package/astra-cli',
 				github: 'https://github.com/astracompiler',
 			},
+			customCss: [
+				'./root.css',
+			],
 			sidebar: [
 				{
 					label: 'CLI',
@@ -19,9 +26,15 @@ export default defineConfig({
 						{ label: 'Introduction', slug: 'cli/introduction' },
 						{ label: 'Installation', slug: 'cli/installation' },
 						{ label: 'Usage', slug: 'cli/usage' },
+						{ label: 'Commands', slug: 'cli/commands' },
+						{ label: 'Configuration', slug: 'cli/configuration' },
+						{ label: 'FAQ', slug: 'cli/faq' },
 					],
 				},
 			],
+			editLink: {
+				baseUrl: 'https://github.com/astracompiler/docs/edit/main/',
+			},
 		}),
 	],
 });
